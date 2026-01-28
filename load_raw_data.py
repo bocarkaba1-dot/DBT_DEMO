850bc9a0aa0a32c270cd4585b848adf4df126a18
bocarkaba1-dot <bocarkaba1@gmail.com>
2026-01-28 10:11:34 +0100

    Mise à jour du projet sans exposer les mots de passe

--- load_raw_data.py
+++ load_raw_data.py
@@ -1,13 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
 # Paramètres de connexion.
-username = "root"
-password = "KABAa&992544"
-host = "localhost"
-port = 3306
-database = "my_dbt_db"
+# Paramètres de connexion (via .env)
+username = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+database = os.getenv("DB_NAME")
+
 
 # On créée la connexion vers la base de données.
 DATABASE_URI = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
